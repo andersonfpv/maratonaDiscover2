69ce172c35d769893bbab20c5d2be707d80dd76b
Anderson Felipe Pereira Vieira <anderson.vieira@santuario.local>
2021-05-06 13:59:28 -0300

    Servidor com express, template conversando com back end

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+// express é uma biblioteca para cirar o servidor
+const express = require('express');
+//router é uma parte que ira criar os caminhos(routes)
+const routes = express.Router()
+
+//views ou tbm pode ser basePath, é o caminho base (onde encontrei as minhas views)
+const views = __dirname + "/views/"
+
+const profile = {
+    name: "Anderson",
+    avatar: "https://github.com/andersonfpv.png",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+
+// request(req), response(res) só encurtei o nome
+routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.post('/job', (req, res) => {
+    console.log(req)
+})
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
+
+
+module.exports = routes;
