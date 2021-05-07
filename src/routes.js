9f0e746b179f6f97cc43c249341da67f2fd041d3
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-05-07 01:15:21 -0300

    Cálculo do tempo restante

--- src/routes.js
+++ src/routes.js
@@ -15,13 +15,59 @@ const profile = {
     "vacation-per-year": 4
 }
 
-const jobs = []
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 2,
+        "total-hours": 60,
+        created_at = Date.now()
+    },
+    {
+        id: 2,
+        name: "OneTwo Projects",
+        "daily-hours": 3,
+        "total-hours": 47,
+        created_at = Date.now()
+    },
+]
+
+
 
 // request(req), response(res) só encurtei o nome
-routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/', (req, res) => {
+    
+    
+    const updatedJobs = jobs.map((job) => {
+        //ajustes no job
+        //cálculo de tempo restante
+        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+
+        const createdDate = new Date(job.created_at)
+        const dueDay = createdDate.getDate() + Number(remainingDays)
+        const dueDateInMs = createdDate.setDate(dueDay)
+
+        const timeDiffInMs = dueDateInMs - Date.now()
+
+        return job
+    })
+    
+    return res.render(views + "index", { jobs })
+})
+
+
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.post('/job', (req, res) => {
-    jobs.push(req.body)
+
+    const lastId = jobs[jobs.length - 1]?.id || 1;
+
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at = Date.now() // atribuindo data de hoje
+    })
     return res.redirect('/')
 })
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
