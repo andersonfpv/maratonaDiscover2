b15ebe263c64a2aaeff876a830409cc860258f64
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-05-09 00:46:29 -0300

    refatoração de código e organização para estrutura mvc

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const server = express()
 const routes = require("./routes")
+const path = require("path")
 
 // usando template engine
 server.set('view engine',  'ejs')
 
-//habilitar arquivos statics
+// Mudar localização da pasta views
+server.set('views', path.join(__dirname, 'views'))
+
+// habilitar arquivos statics
 server.use(express.static("public"))
 
 // usar o req.body
